75a5cfefe281be134284fecd688a649c92451bff
Serge Najimov <najimovserge@gmail.com>
2020-08-19 17:28:17 +0200

    serving HTML pages with static CSS

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Main Screen</h1>');
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
@@ -7,13 +8,13 @@ const shopRouter = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRouter);
-
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
