99e29bf60f02e75f2dc4e4c88457a223e0e5f80e
Serge Najimov <najimovserge@gmail.com>
2020-08-20 11:24:01 +0200

    Done with Pug

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', { pageTitle: 'Page not found' });
 });
 
 app.listen(3000);
