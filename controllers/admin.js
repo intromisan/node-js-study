ef086a7c9f3493d711939168e1004e80f0fd480a
Serge Najimov <najimovserge@gmail.com>
2020-08-24 17:48:33 +0200

    Connected MYSQL Database, chaged some product methods to get data from mysql database

--- controllers/admin.js
+++ controllers/admin.js
@@ -37,28 +37,30 @@ exports.getEditProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
-  const description = req.body.description;
   const price = req.body.price;
+  const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product
+    .save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
-  const updatedImageUrl = req.body.imageUrl;
   const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
   const updatedDescription = req.body.description;
-  const updatedProduct = new Product(
+  const product = new Product(
     prodId,
     updatedTitle,
-    updatedImageUrl,
+    updatedPrice,
     updatedDescription,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('products');
+    updatedImageUrl
+  );  
 };
 
 exports.postDeleteProduct = (req, res, next) => {
--- controllers/shop.js
+++ controllers/shop.js
@@ -2,44 +2,51 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/products',
-    });
-  });
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render('shop/product-list', {
+        prods: rows,
+        pageTitle: 'Shop',
+        path: '/products',
+      });
+    })
+    .catch(err => console.log(err));
 }
 
 exports.getProduct = (req, res, next) => {
   const productId = req.params.productId;
-  Product.findById(productId, product => {
-    res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
-  })
+  Product.findById(productId)
+    .then(([product]) => {
+      res.render('shop/product-detail', {
+        product: product[0],
+        pageTitle: product.title,
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err));
 }
 
 exports.getHome = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Home',
-      path: '/',
-    });
-  });
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render('shop/index', {
+        prods: rows,
+        pageTitle: 'Home',
+        path: '/',
+      });
+    })
+    .catch(err => console.log(err));
+
 }
 
 exports.getCart = (req, res, next) => {
   Cart.getCart(cart => {
     Product.fetchAll(products => {
-      const cartProducts =[];
+      const cartProducts = [];
       for (product of products) {
         const cartProductData = cart.products.find(prod => prod.id === product.id)
         if (cartProductData) {
-          cartProducts.push({productData: product, qty: cartProductData.qty});
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
         }
       }
       res.render('shop/cart', {
