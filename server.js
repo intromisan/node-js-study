e3966371e1e94eba1b77d10fc23cee11cf681a86
Serge Najimov <najimovserge@gmail.com>
2020-10-14 21:21:47 +0200

    created User schema and model with mongoose

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const errorController = require('./controllers/error');
 const mongoose = require('mongoose');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,16 +19,16 @@ const shopRouter = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findById('5f7848744f8ab40be53567de')
-//         .then(user => {
-//             req.user = new User(user.name, user.email, user.cart, user._id);
-//             next();
-//         })
-//         .catch(err => {
-//             console.log(err);
-//         })
-// });
+app.use((req, res, next) => {
+    User.findById('5f8749f055fb701d24c54a84')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => {
+            console.log(err);
+        })
+});
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
@@ -37,6 +37,19 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://serge:Pa8633004329@cluster0.s0kyp.mongodb.net/shop?authSource=admin&replicaSet=atlas-k4r12x-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
     .then(res => {
+        User.findOne()
+            .then(user => {
+                if (!user) {
+                    const user = new User({
+                        name: 'Serge',
+                        email: 'serge@test.com',
+                        cart: {
+                            items: []
+                        }
+                    });
+                    user.save();
+                }
+            })
         app.listen(3000);
         console.log('Connected')
     })
